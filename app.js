0aaf582fbfccad2ce6a23127e1c2daf86a15496b
Nenad Benke <nenad.ben87@gmail.com>
2022-07-26 15:46:06 +0200

    FAQ files to commit

---
+++ app.js
@@ -0,0 +1,71 @@
+
+///////// First Question
+const arrowDown1 = document.querySelector('.arrow-down-one');
+const arrowUp1 = document.querySelector('.arrow-up-one');
+const faqAnswer1 = document.getElementById('faq-answer-one');
+
+arrowDown1.addEventListener('click', () => {
+  arrowDown1.style.display = 'none';
+  arrowUp1.style.display = 'block';
+  faqAnswer1.style.display = 'block';
+}) 
+
+arrowUp1.addEventListener('click', () => {
+  arrowUp1.style.display = 'none';
+  arrowDown1.style.display = 'block';
+  faqAnswer1.style.display = 'none';
+})
+
+
+/////////////// Second Question
+const arrowDown2 = document.querySelector('.arrow-down-two');
+const arrowUp2 = document.querySelector('.arrow-up-two');
+const faqAnswer2 = document.getElementById('faq-answer-two');
+
+arrowDown2.addEventListener('click', () => {
+  arrowDown2.style.display = 'none';
+  arrowUp2.style.display = 'block';
+  faqAnswer2.style.display = 'block';
+}) 
+
+arrowUp2.addEventListener('click', () => {
+  arrowUp2.style.display = 'none';
+  arrowDown2.style.display = 'block';
+  faqAnswer2.style.display = 'none';
+})
+
+
+///////// Third Question
+const arrowDown3 = document.querySelector('.arrow-down-three');
+const arrowUp3 = document.querySelector('.arrow-up-three');
+const faqAnswer3 = document.getElementById('faq-answer-three');
+
+arrowDown3.addEventListener('click', () => {
+  arrowDown3.style.display = 'none';
+  arrowUp3.style.display = 'block';
+  faqAnswer3.style.display = 'block';
+}) 
+
+arrowUp3.addEventListener('click', () => {
+  arrowUp3.style.display = 'none';
+  arrowDown3.style.display = 'block';
+  faqAnswer3.style.display = 'none';
+})
+
+
+///////// Fourth Question
+const arrowDown4 = document.querySelector('.arrow-down-four');
+const arrowUp4 = document.querySelector('.arrow-up-four');
+const faqAnswer4 = document.getElementById('faq-answer-four');
+
+arrowDown4.addEventListener('click', () => {
+  arrowDown4.style.display = 'none';
+  arrowUp4.style.display = 'block';
+  faqAnswer4.style.display = 'block';
+}) 
+
+arrowUp4.addEventListener('click', () => {
+  arrowUp4.style.display = 'none';
+  arrowDown4.style.display = 'block';
+  faqAnswer4.style.display = 'none';
+})
